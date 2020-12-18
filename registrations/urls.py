726529efca89faa1e93ebaf3f6fa08e3355d6b7f
lhggomes <lucas.henrique.s.go@gmail.com>
2020-12-18 07:58:13 -0300

    Adding a page for the control of the targets

--- registrations/urls.py
+++ registrations/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TargetCrateView
+from .views import TargetCrateView, TargetList
 
 urlpatterns = [
     path('registration/target/', TargetCrateView.as_view(), name='reg-target'),
+    path('targets/', TargetList.as_view(), name='targets'),
 
 ]
